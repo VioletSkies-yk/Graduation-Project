d37905e93e45298b36d84fedb069b1ecd96bcccf
YkAtHome <1160255605@qq.com>
2023-03-20 02:00:30 +0800

    电梯门

---
+++ Assets/Scripts/GamePlay/GameLogic/Episode1/ElevatorTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using DG.Tweening;
+using Assets.Scripts.GamePlay.GameLogic.Episode1;
+
+namespace Assets.Scripts.GamePlay.GameLogic
+{
+    public class ElevatorTrigger : TouchTrigger
+    {
+        /// <summary>
+        /// 电梯左门
+        /// </summary>
+        [Space]
+        [Header("电梯左门")]
+        [SerializeField] private Transform _elevatorLeftDoor;
+
+        /// <summary>
+        /// 电梯右门
+        /// </summary>
+        [Space]
+        [Header("电梯右门")]
+        [SerializeField] private Transform _elevatorRightDoor;
+
+        private bool isFirst = true;
+
+        public override void OnTriggerEnterCallBack()
+        {
+            //_elevatorLeftDoor.DOMove();
+            //_elevatorRightDoor.DOMove();
+        }
+    }
+}
